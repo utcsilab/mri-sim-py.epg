5ff6e8ecb1b8510bf30d662017527386ed6957ea
Somnath Rakshit <12930218+somnathrakshit@users.noreply.github.com>
2020-12-15 06:12:26 +0000

    Change simulator name to EPG

--- plot.py
+++ plot.py
@@ -14,15 +14,16 @@ ax = plt.axes()
 # y = df[df['Simulator'].str.contains('New', na = False)]
 linestyles = ['solid', 'dashed', 'dashdot', 'dotted']
 i = 0
-for simulator in ["New", "Old"]:
+sims = {"Parallelized": "New", "Naive": "Old"}
+for simulator in ["Parallelized", "Naive"]:
     for device in ["cpu", "cuda"]:
-        y = df[df['Simulator'].str.match(simulator) & df['Device'].str.match(device)]
+        y = df[df['Simulator'].str.match(sims[simulator]) & df['Device'].str.match(device)]
         x = y.loc[:, 'Batch Size']
         y = y.loc[:, 'Time']
-        plt.plot(x, np.log10(y), label=f'{simulator} simulator on {device}')
+        plt.plot(x, np.log10(y), label=f'{simulator} EPG on {device}')
         i += 1
 
-plt.legend(loc=9, bbox_to_anchor=(1.17, 0.65), frameon=True)
+plt.legend(loc=9, bbox_to_anchor=(1.2, 0.65), frameon=True)
 plt.xlabel('Batch Size')
 plt.ylabel('$\log_{10}$ Time')
 plt.title('Batch size vs. $\log_{10}$ time')
@@ -34,14 +35,15 @@ fig = plt.figure()
 ax = plt.axes()
 linestyles = ['solid', 'dashdot', 'dashed', 'dotted']
 i = 0
-for simulator in ["New", "Old"]:
+sims = {"Parallelized": "New", "Naive": "Old"}
+for simulator in ["Parallelized", "Naive"]:
     for device in ["cpu", "cuda"]:
-        y = df[df['Simulator'].str.match(simulator) & df['Device'].str.match(device)]
+        y = df[df['Simulator'].str.match(sims[simulator]) & df['Device'].str.match(device)]
         x = y.loc[:, 'Batch Size']
         y = y.loc[:, 'Speed']
-        plt.plot(x, y, label=f'{simulator} simulator on {device}', linestyle=linestyles[i])
+        plt.plot(x, y, label=f'{simulator} EPG on {device}', linestyle=linestyles[i])
         i += 1
-plt.legend(loc=9, bbox_to_anchor=(1.17, 0.65), frameon=True)
+plt.legend(loc=9, bbox_to_anchor=(1.2, 0.65), frameon=True)
 plt.xlabel('Batch Size')
 plt.ylabel('Speed in microseconds')
 plt.title('Batch size vs. Speed in microseconds')
